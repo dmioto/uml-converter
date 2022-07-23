934af9fea7e6158bd5c11facc111321d2cf6b624
Daniel D. Mioto <daniel.mioto@outlook.com>
2022-07-23 01:51:37 -0300

    first commit

---
+++ uml-converter/src/main/java/br/com/unesp/uml_converter/models/BaseAttr.java
@@ -0,0 +1,54 @@
+package br.com.unesp.uml_converter.models;
+
+public class BaseAttr {
+    
+    private String modifier;
+    private String type;
+    private String name;
+    private String value;
+
+    public BaseAttr(String modifier, String type, String name, String value) {
+        this.modifier = modifier;
+        this.type = type;
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getModifier() {
+        return modifier;
+    }
+
+    public void setModifier(String modifier) {
+        this.modifier = modifier;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseAttr{" + "modifier=" + modifier + ", type=" + type + ", name=" + name + ", value=" + value + '}';
+    }
+    
+}
---
+++ uml-converter/src/main/java/br/com/unesp/uml_converter/models/Project.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.unesp.uml_converter.models;
+
+import java.util.List;
+
+public class Project {
+
+    private String projectName;
+    private List<BaseClass> objects;
+
+    public Project(String projectName, List<BaseClass> objects) {
+        this.projectName = projectName;
+        this.objects = objects;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public List<BaseClass> getObjects() {
+        return objects;
+    }
+
+    public void setObjects(List<BaseClass> objects) {
+        this.objects = objects;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" + "projectName=" + projectName + ", objects=" + objects.toString() + '}';
+    }
+
+}
