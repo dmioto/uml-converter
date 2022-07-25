194b08b58288534bf348a07b14174b0f9eb3fcfc
Diogo Paiva Pereira <diogo.p.pereira@unesp.br>
2022-07-25 15:54:50 -0300

    Functional

--- uml-converter/src/main/java/br/com/unesp/uml_converter/utils/UmlUtils.java
+++ uml-converter/src/main/java/br/com/unesp/uml_converter/utils/UmlUtils.java
@@ -10,13 +10,12 @@ import br.com.unesp.uml_converter.models.BaseProject;
 import br.com.unesp.uml_converter.models.BaseRelationship;
 import java.util.List;
 
-
 public class UmlUtils {
-    
+
     public static String projectToUML(BaseProject project) {
-        
+
         StringBuilder strUML = new StringBuilder();
-        
+
         strUML.append("@startuml");
         strUML.append(System.lineSeparator());
         strUML.append(String.format("Title: %s", project.getProjectName()));
@@ -25,14 +24,14 @@ public class UmlUtils {
         strUML.append(relationshipToUML(project.getRelationships()));
         strUML.append(System.lineSeparator());
         strUML.append("@enduml");
-        
+
         return strUML.toString();
     }
-    
+
     private static String objectToUml(List<BaseClass> objects) {
-    
+
         StringBuilder objectsUML = new StringBuilder();
-        
+
         for (BaseClass o : objects) {
             objectsUML.append(System.lineSeparator());
             objectsUML.append(String.format("class %s {", o.getObjectName()));
@@ -40,87 +39,100 @@ public class UmlUtils {
             objectsUML.append(attributeToUml(o.getAttributes()));
             objectsUML.append("}");
         }
-        
+
         return objectsUML.toString();
     }
-    
+
     private static String attributeToUml(List<BaseAttr> attributes) {
-    
+
         StringBuilder attrsUML = new StringBuilder();
-        
+
         for (BaseAttr attr : attributes) {
-            attrsUML.append(String.format("    %s %s %s", verifyModifier(attr.getModifier()), attr.getType(), attr.getName()));
+            attrsUML.append(
+                    String.format("    %s %s %s", verifyModifier(attr.getModifier()), attr.getType(), attr.getName()));
             attrsUML.append(System.lineSeparator());
         }
-        
+
         return attrsUML.toString();
     }
-    
+
     private static String relationshipToUML(List<BaseRelationship> relationships) {
-        
+
         StringBuilder relationUML = new StringBuilder();
-        
+
         for (BaseRelationship attr : relationships) {
-            relationUML.append(String.format("%s %s %s", attr.getObject1(), verifyRelantionshipMultiplicity(attr) , attr.getObject2()));
+            relationUML.append(String.format("%s %s %s", attr.getObject1(), verifyRelantionshipMultiplicity(attr),
+                    attr.getObject2()));
+            relationUML.append(System.lineSeparator());
         }
-        
+
         return relationUML.toString();
     }
-    
+
     private static String verifyModifier(String v) {
-        
+
         String modifier = " ";
-        
+
         switch (v) {
             case "public":
                 modifier = "+";
                 break;
-                
+
             case "private":
-                modifier =  "-";
+                modifier = "-";
                 break;
-                
+
             case "protected":
-                modifier =  "#";
+                modifier = "#";
                 break;
-                
+
             default:
                 throw new AssertionError();
         }
-        
+
         return modifier;
     }
-    
+
     private static String verifyRelantionshipMultiplicity(BaseRelationship baseRM) {
-                
-        String strRM = String.format("%s %s %s", baseRM.getObject1(), verifyRelation(baseRM.getRelation()), baseRM.getObject2());
+
+        String strRM = "";
+
+        if (!baseRM.getMultiplicity1().equals("none")) {
+            strRM = String.format("\"%s\" ", baseRM.getMultiplicity1());
+        }
+
+        strRM = strRM + String.format("%s", verifyRelation(baseRM.getRelation()));
+
+        if (!baseRM.getMultiplicity2().equals("none")) {
+            strRM = strRM + String.format(" \"%s\"", baseRM.getMultiplicity2());
+        }
         
         return strRM;
     }
-    
+
     public static String verifyRelation(String m) {
-    
+
         String rm = " ";
-        
+
         switch (m) {
             case "inheritance":
                 rm = "--|>";
                 break;
-                
+
             case "composition":
-                rm =  "--*";
+                rm = "--*";
                 break;
-                
+
             case "agregation":
-                rm =  "--o" ;
+                rm = "--o";
                 break;
-                
+
             default:
                 throw new AssertionError();
         }
-        
+
         return rm;
-        
-    }   
-    
+
+    }
+
 }
